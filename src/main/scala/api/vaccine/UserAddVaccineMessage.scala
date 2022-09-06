a4c20ca6357753f9a42d881fcdd5af5fb977ec76
yhx-12243 <yhx0706@163.com>
2022-09-06 18:55:36 +0800

    feat: vaccine microservice test passed

--- src/main/scala/api/vaccine/UserAddVaccineMessage.scala
+++ src/main/scala/api/vaccine/UserAddVaccineMessage.scala
@@ -1,6 +1,8 @@
 package api.vaccine
 
-import scala.util.Try
+import scala.util.{Success, Try}
+
+import api.exotic.CheckAccessPermission
 import api.{TSMSPMessage, TSMSPReply}
 import models.fields.{IDCard, MicroServiceToken}
 import models.types.JacksonSerializable
@@ -8,8 +10,17 @@ import utils.{MicroServicePorts, MicroServiceTokens}
 import utils.MicroServicePorts.Port
 import utils.http.sender
 
+case class AddVaccine(secret: MicroServiceToken, idCard: IDCard, manufacture: String, time: Long, `type`: String = "AddVaccine") extends JacksonSerializable
+
 case class UserAddVaccineMessage(userToken: String, idCard: IDCard, manufacture: String, time: Long) extends TSMSPMessage {
-//  override def reaction(now: DateTime): Try[TSMSPReply] = Try {
-//    TSMSPReply(HandleStatus.OK, addVaccine(userToken, idCard, manufacture, time, now).get)
-//  }
+  override def reaction(): Try[TSMSPReply] = Try {
+    CheckAccessPermission(MicroServiceTokens.impl.user, userToken, idCard)
+      .send[TSMSPReply](MicroServicePorts.user.APIUrl) match {
+      case Success(response) if response.status == 0 =>
+      case other => return other
+    }
+    AddVaccine(MicroServiceTokens.impl.vaccine, idCard, manufacture, time)
+      .send[TSMSPReply](MicroServicePorts.vaccine.APIUrl)
+      .get
+  }
 }
--- src/main/scala/api/vaccine/UserGetVaccineMessage.scala
+++ src/main/scala/api/vaccine/UserGetVaccineMessage.scala
@@ -1,6 +1,8 @@
 package api.vaccine
 
-import scala.util.Try
+import scala.util.{Success, Try}
+
+import api.exotic.CheckAccessPermission
 import api.{TSMSPMessage, TSMSPReply}
 import models.fields.{IDCard, MicroServiceToken}
 import models.types.JacksonSerializable
@@ -8,8 +10,17 @@ import utils.{MicroServicePorts, MicroServiceTokens}
 import utils.MicroServicePorts.Port
 import utils.http.sender
 
+case class GetVaccine(secret: MicroServiceToken, idCard: IDCard, `type`: String = "GetVaccine") extends JacksonSerializable
+
 case class UserGetVaccineMessage(userToken: String, idCard: IDCard) extends TSMSPMessage {
-//  override def reaction(now: DateTime): Try[TSMSPReply] = Try {
-//    TSMSPReply(HandleStatus.OK, getVaccines(userToken, idCard, now).get)
-//  }
+  override def reaction(): Try[TSMSPReply] = Try {
+    CheckAccessPermission(MicroServiceTokens.impl.user, userToken, idCard)
+      .send[TSMSPReply](MicroServicePorts.user.APIUrl) match {
+      case Success(response) if response.status == 0 =>
+      case other => return other
+    }
+    GetVaccine(MicroServiceTokens.impl.vaccine, idCard)
+      .send[TSMSPReply](MicroServicePorts.vaccine.APIUrl)
+      .get
+  }
 }
